2be3c096007478610ad7e4368432e0032b310936
sajjad-salam <sajjad.salam.teama@gmail.com>
2024-07-14 12:34:58 +0300

    "Updated README.md with new content and reformatted main.py code with improved indentation and added import statements"

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
-import requests,random
+import requests
+import random
 from getuseragent import UserAgent
+
 ua = UserAgent("ios").Random()
-user=input('[+] TikTok UserName : ')
-link=input('[+] Video Link : ')
-res=requests.post('https://api.likesjet.com/freeboost/3',json={"link":link,"tiktok_username":user,"email":f"whisper{random.randint(100000,999999)}@gmail.com"},headers={"Host": "api.likesjet.com","content-length": "137","sec-ch-ua": "\"Google Chrome\";v\u003d\"119\", \"Chromium\";v\u003d\"119\", \"Not?A_Brand\";v\u003d\"24\"","accept": "application/json, text/plain, */*","content-type": "application/json","sec-ch-ua-mobile": "?1","user-agent":ua,"sec-ch-ua-platform": "\"Android\"","origin": "https://likesjet.com","sec-fetch-site": "same-site","sec-fetch-mode": "cors","sec-fetch-dest": "empty","referer": "https://likesjet.com/","accept-language": "en-XA,en;q\u003d0.9,ar-XB;q\u003d0.8,ar;q\u003d0.7,en-GB;q\u003d0.6,en-US;q\u003d0.5"}).json()
+user = input("[+] TikTok UserName : ")
+link = input("[+] Video Link : ")
+res = requests.post(
+    "https://api.likesjet.com/freeboost/3",
+    json={
+        "link": link,
+        "tiktok_username": user,
+        "email": f"whisper{random.randint(100000,999999)}@gmail.com",
+    },
+    headers={
+        "Host": "api.likesjet.com",
+        "content-length": "137",
+        "sec-ch-ua": '"Google Chrome";v\u003d"119", "Chromium";v\u003d"119", "Not?A_Brand";v\u003d"24"',
+        "accept": "application/json, text/plain, */*",
+        "content-type": "application/json",
+        "sec-ch-ua-mobile": "?1",
+        "user-agent": ua,
+        "sec-ch-ua-platform": '"Android"',
+        "origin": "https://likesjet.com",
+        "sec-fetch-site": "same-site",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-dest": "empty",
+        "referer": "https://likesjet.com/",
+        "accept-language": "en-XA,en;q\u003d0.9,ar-XB;q\u003d0.8,ar;q\u003d0.7,en-GB;q\u003d0.6,en-US;q\u003d0.5",
+    },
+).json()
 print(res)
